977a9897b9a576fde406dc90b2d8604141dd3e95
vetri <vetrivel.tcz@gmail.com>
2024-11-25 12:52:13 +0530

    new

--- src/Shared/Components/TamilSchools/TamilSchools.jsx
+++ src/Shared/Components/TamilSchools/TamilSchools.jsx
@@ -9,7 +9,7 @@ const VideoGallery = (props) => {
             </div>
             <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
                 {TamilSchool.map((video) => (
-                    <div key={video.id} className={`cursor-pointer     border  rounded-lg  hover:border-blue-500 ${currentVideo.id === video.id ? 'border-blue-500' : 'border-gray-300'}`} onClick={() => handleVideoClick(video)}  >
+                    <div key={video.id} className={`cursor-pointer     border  rounded-lg    ${currentVideo.id === video.id ? ' ' : 'border-gray-300'}`} onClick={() => handleVideoClick(video)}  >
                         <iframe className='w-full h-44 object-cover   rounded-tr-lg rounded-tl-lg ' height="720" src={video.src} title="STM October 2022 - பைந்தமிழ் வாசகர் வட்டம்"  ></iframe>
                         <p className=" p-3 text-center text-white concert-one-regular bg-gradient-to-l from-[#0571BC] to-[#E91E31] rounded-br-lg rounded-bl-lg"  >{video.title}</p>
                     </div>
